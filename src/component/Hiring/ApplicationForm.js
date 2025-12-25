981bc96eccf9ebcd380fe07397dae9a4e9947330
FATECHANGE2024 <rajm43439@gmail.com>
2025-12-25 14:53:17 +0530

    Completed Hiring

--- src/component/Hiring/ApplicationForm.js
+++ src/component/Hiring/ApplicationForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { X, Upload, Check } from 'lucide-react';
 
 const positions = [
+  'Graphic Designer',
   'Next.js Developer',
   'Flutter Developer',
   'Android Developer',
