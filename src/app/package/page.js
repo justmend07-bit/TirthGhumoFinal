bd12fe096284ad4ac740f08b88747f600d4b77e4
FATECHANGE2024 <rajm43439@gmail.com>
2026-01-14 15:21:57 +0530

    added nav to package

--- src/app/package/page.js
+++ src/app/package/page.js
@@ -1,9 +1,11 @@
 import PackagesClient from '@/component/package/packageSection';
 import Footer from '@/component/footer';
+import Navbar from "@/component/navbar";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-amber-50 via-orange-50 to-amber-50">
+    <div className="min-h-screen bg-gradient-to-br from-amber-50 via-orange-50 to-amber-50 ">
+      <Navbar />
       <PackagesClient />
             <Footer />
 
